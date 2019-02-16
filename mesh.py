cf1f1ea4a3056096ed76b5dfdbf33e5d95cfe25a
ahmedmdl <43715890+ahmedmdl@users.noreply.github.com>
2019-02-16 05:59:02 +0200

    Update mesh.py

--- mesh.py
+++ mesh.py
@@ -3,6 +3,7 @@ import numpy
 from stl import mesh
 import pandas as pd
 import csv
+import subprocess
 
 x_Tolerance=1.5
 y_Tolerance=1.5
@@ -41,5 +42,25 @@ for i in range(2,len(x)):
    meshh.translate([x[i-1]/x_Tolerance,y[i-1]/y_Tolerance,z[i-1]/z_Tolerance])
    mesH.append(meshh)
 combined = mesh.Mesh(numpy.concatenate([main_body.data]+[meshee.data for meshee in mesH]))
-combined.save('../Finale/combined.stl')
+
+#task = subprocess.Popen("ls ../Finale -1 | wc -l",shell=True,stdout=subprocess.PIPE)
+#No_mesh = task.stdout.read()
+#No_mesh = int(No_mesh)
+task = subprocess.Popen("ls ../Finale -1v",shell=True,stdout=subprocess.PIPE)
+last_mesh = task.stdout.read()
+if len(last_mesh) == 0:
+        with open("../Finale/combined_001.txt", 'w+') as f:
+                  f.write("x_tolerance ="+ str(x_Tolerance)+
+                          "\ny_tolerance ="+str(y_Tolerance)+
+                          "\nz_tolerance ="+str(z_Tolerance))
+                  
+        combined.save('../Finale/combined_001.stl')        
+else:
+    print(last_mesh)    
+    last_mesh = int(last_mesh[-8:-5])
+    with open("../Finale/combined_%03d.txt"%(last_mesh+1), 'w+') as f:
+                  f.write("x_tolerance ="+ str(x_Tolerance)+
+                          "\ny_tolerance ="+str(y_Tolerance)+
+                          "\nz_tolerance ="+str(z_Tolerance))
+    combined.save('../Finale/combined_%03d.stl'%(last_mesh+1))
 #v3,c3,i3 = meshhh.get_mass_properties()
